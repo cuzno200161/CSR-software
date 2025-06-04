b4f62c8649fa9a99bc89ac02f289e9b31efc08e5
Ming Lo <miyulo135@gmail.com>
2025-06-04 13:00:05 -0700

    Finished OACD class: added factors 4-10, modifying/changing extrenum

--- OACD.py
+++ OACD.py
@@ -23,14 +23,30 @@ class OACD:
     def set_factor_num(self, factor_num):
         self.factor_num = factor_num
         
+        # When factor number is changed, the extrenum table is automatically changed too
+        self.factor_extrenum = pd.DataFrame(np.ones((self.factor_num, 2)))
+        self.factor_extrenum.iloc[:, 1] = -1; 
+        
     def set_table_size(self, table_size):
         self.table_size = table_size
         
+    def set_factor_extrenum(self, new_extrenum):
+        # Set the entire extrenum table (can be used if importing a table of extrenum)
+        self.factor_extrenum = new_extrenum
+        
+    def set_extrenum(self, value, factor, extrenum):
+        # Set a singular extrenum value (can be used for manual input of extrenum)
+        if extrenum == "min":
+            self.factor_extrenum.iloc[factor, 0] = value;
+        elif extrenum == "max":
+            self.factor_extrenum.iloc[factor, 1] = value;
+        
     def excel_to_python(self, filepath):
         df = pd.read_excel(filepath, header=None)
-        return df
+        return df    
          
     def build_table(self):
+        
         # Returns -1 if there table isn't built / error in factor_num
         if(self.table_size != "Small" and self.table_size != "Medium" and self.table_size != "Large"):
             return -1
@@ -69,69 +85,195 @@ class OACD:
                     case "Small":
                         t1 = self.excel_to_python("OACD_tables/2_5-2.xlsx")
                         t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
-                        t2 = t2.iloc[:, [1, 2, 3, 5]]
+                        t2.iloc[:, 0] = t2.iloc[:, 1]
+                        t2.iloc[:, 1] = t2.iloc[:, 2]
+                        t2.iloc[:, 2] = t2.iloc[:, 3]
+                        t2.iloc[:, 3] = t2.iloc[:, 5]
                         self.table = pd.concat([t1, t2.iloc[:, :5]], ignore_index=True)
                     case "Medium":
                         t1 = self.excel_to_python("OACD_tables/pb12.xlsx")
                         t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
-                        t2 = t2.iloc[:, [1, 4, 2, 3, 5]]
-                        self.table = pd.concat([t1, t2.iloc[:, :5]], ignore_index=True)
+                        t2.iloc[:, 0] = t2.iloc[:, 1]
+                        t2.iloc[:, 1] = t2.iloc[:, 4]
+                        t2.iloc[:, 4] = t2.iloc[:, 5]
+                        self.table = pd.concat([t1.iloc[:, :5], t2.iloc[:, :5]], ignore_index=True)
                     case "Large":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_5-1.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        t2.columns = [col - 1 for col in t2.columns] # Shift columns names to left by 1
+                        self.table = pd.concat([t1, t2.iloc[:, 1:6]], ignore_index=True)
             case 6:
                 match(self.table_size):
                     case "Small":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/pb12.xlsx")
+                        t1.iloc[:, 5] = t1.iloc[:, 6]
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 0] = t2.iloc[:, 1]
+                        t2r.iloc[:, 1] = t2.iloc[:, 4]
+                        t2r.iloc[:, 4] = t2.iloc[:, 5]
+                        t2r.iloc[:, 5] = t2.iloc[:, 0]
+                        self.table = pd.concat([t1.iloc[:, :6], t2r.iloc[:, :6]], ignore_index=True)
                     case "Medium":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/pb20.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 1] = t2.iloc[:, 3]
+                        t2r.iloc[:, 2] = t2.iloc[:, 5]
+                        t2r.iloc[:, 3] = t2.iloc[:, 2]
+                        t2r.iloc[:, 4] = t2.iloc[:, 1]
+                        t2r.iloc[:, 5] = t2.iloc[:, 4]
+                        self.table = pd.concat([t1.iloc[:, :6], t2r.iloc[:, :6]], ignore_index=True)
                     case "Large":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_6-1.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        self.table = pd.concat([t1, t2.iloc[:, :6]], ignore_index=True)
+                        
             case 7:
                 match(self.table_size):
                     case "Small":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/pb20.xlsx")
+                        t1.iloc[:, 5] = t1.iloc[:, 12]
+                        t1.iloc[:, 6] = t1.iloc[:, 15]
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 0] = t2.iloc[:, 2]
+                        t2r.iloc[:, 1] = t2.iloc[:, 0]
+                        t2r.iloc[:, 2] = t2.iloc[:, 4]
+                        t2r.iloc[:, 3] = t2.iloc[:, 6]
+                        t2r.iloc[:, 4] = t2.iloc[:, 3]
+                        t2r.iloc[:, 5] = t2.iloc[:, 1]
+                        t2r.iloc[:, 6] = t2.iloc[:, 5]
+                        self.table = pd.concat([t1.iloc[:, :7], t2r.iloc[:, :7]], ignore_index=True)
                     case "Medium":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_6-2.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 2] = t2.iloc[:, 4]
+                        t2r.iloc[:, 3] = t2.iloc[:, 2]
+                        t2r.iloc[:, 4] = t2.iloc[:, 3]
+                        t2r.iloc[:, 5] = t2.iloc[:, 6]
+                        t2r.iloc[:, 6] = t2.iloc[:, 5]
+                        self.table = pd.concat([t1, t2r.iloc[:, :7]], ignore_index=True)
                     case "Large":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_7-1.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa18.xlsx")
+                        self.table = pd.concat([t1, t2.iloc[:, :7]], ignore_index=True)
             case 8:
                 match(self.table_size):
                     case "Small":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/pb20.xlsx")
+                        t1.iloc[:, 5] = t1.iloc[:, 12]
+                        t1.iloc[:, 6] = t1.iloc[:, 15]
+                        t1.iloc[:, 7] = t1.iloc[:, 14]
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 0] = t2.iloc[:, 5]
+                        t2r.iloc[:, 1] = t2.iloc[:, 2]
+                        t2r.iloc[:, 2] = t2.iloc[:, 7]
+                        t2r.iloc[:, 4] = t2.iloc[:, 1]
+                        t2r.iloc[:, 5] = t2.iloc[:, 0]
+                        t2r.iloc[:, 7] = t2.iloc[:, 4]
+                        self.table = pd.concat([t1.iloc[:, :8], t2r.iloc[:, :8]], ignore_index=True)
                     case "Medium":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_8-3.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 1] = t2.iloc[:, 2]
+                        t2r.iloc[:, 2] = t2.iloc[:, 3]
+                        t2r.iloc[:, 3] = t2.iloc[:, 4]
+                        t2r.iloc[:, 4] = t2.iloc[:, 1]
+                        t2r.iloc[:, 5] = t2.iloc[:, 6]
+                        t2r.iloc[:, 6] = t2.iloc[:, 7]
+                        t2r.iloc[:, 7] = t2.iloc[:, 5]
+                        self.table = pd.concat([t1, t2r.iloc[:, :8]], ignore_index=True)
                     case "Large":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_8-2.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        self.table = pd.concat([t1, t2.iloc[:, :8]], ignore_index=True)
             case 9:
                 match(self.table_size):
                     case "Small":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_9-4.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 0] = t2.iloc[:, 4]
+                        t2r.iloc[:, 1] = t2.iloc[:, 5]
+                        t2r.iloc[:, 2] = t2.iloc[:, 0]
+                        t2r.iloc[:, 3] = t2.iloc[:, 6]
+                        t2r.iloc[:, 4] = t2.iloc[:, 1]
+                        t2r.iloc[:, 5] = t2.iloc[:, 3]
+                        t2r.iloc[:, 6] = t2.iloc[:, 8]
+                        t2r.iloc[:, 7] = t2.iloc[:, 2]
+                        t2r.iloc[:, 8] = t2.iloc[:, 7]
+                        self.table = pd.concat([t1, t2r.iloc[:, :9]], ignore_index=True)
                     case "Medium":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_9-3.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 1] = t2.iloc[:, 2]
+                        t2r.iloc[:, 2] = t2.iloc[:, 7]
+                        t2r.iloc[:, 3] = t2.iloc[:, 1]
+                        t2r.iloc[:, 4] = t2.iloc[:, 5]
+                        t2r.iloc[:, 5] = t2.iloc[:, 6]
+                        t2r.iloc[:, 6] = t2.iloc[:, 4]
+                        t2r.iloc[:, 7] = t2.iloc[:, 3]
+                        self.table = pd.concat([t1, t2r.iloc[:, :9]], ignore_index=True)
                     case "Large":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_9-2.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        self.table = pd.concat([t1, t2.iloc[:, :9]], ignore_index=True)
             case 10:
                 match(self.table_size):
                     case "Small":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_10-5.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 0] = t2.iloc[:, 6]
+                        t2r.iloc[:, 1] = t2.iloc[:, 5]
+                        t2r.iloc[:, 3] = t2.iloc[:, 1]
+                        t2r.iloc[:, 4] = t2.iloc[:, 8]
+                        t2r.iloc[:, 5] = t2.iloc[:, 0]
+                        t2r.iloc[:, 6] = t2.iloc[:, 9]
+                        t2r.iloc[:, 8] = t2.iloc[:, 4]
+                        t2r.iloc[:, 9] = t2.iloc[:, 3]
+                        self.table = pd.concat([t1, t2r.iloc[:, :10]], ignore_index=True)
                     case "Medium":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_10-4.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        t2r = t2
+                        t2r.iloc[:, 0] = t2.iloc[:, 4]
+                        t2r.iloc[:, 1] = t2.iloc[:, 6]
+                        t2r.iloc[:, 2] = t2.iloc[:, 7]
+                        t2r.iloc[:, 3] = t2.iloc[:, 1]
+                        t2r.iloc[:, 4] = t2.iloc[:, 2]
+                        t2r.iloc[:, 5] = t2.iloc[:, 3]
+                        t2r.iloc[:, 6] = t2.iloc[:, 9]
+                        t2r.iloc[:, 7] = t2.iloc[:, 6]
+                        t2r.iloc[:, 8] = t2.iloc[:, 8]
+                        t2r.iloc[:, 9] = t2.iloc[:, 0]
+                        self.table = pd.concat([t1, t2r.iloc[:, :10]], ignore_index=True)
                     case "Large":
-                        pass;
+                        t1 = self.excel_to_python("OACD_tables/2_10-3.xlsx")
+                        t2 = self.excel_to_python("OACD_tables/oa27.xlsx")
+                        self.table = pd.concat([t1, t2.iloc[:, :10]], ignore_index=True)
             case _:
                 # Return -2 if factor_num is not in valid range
                 return -2;
         
+        # Change the three-level design into the min, max, and average of each factor
+        for factor in range(self.factor_num):
+            self.table.iloc[:, factor].replace(1, self.factor_extrenum.iloc[factor, 1])
+            self.table.iloc[:, factor].replace(-1, self.factor_extrenum.iloc[factor, 0])
+            self.table.iloc[:, factor].replace(0, (self.factor_extrenum.iloc[factor, 1] + self.factor_extrenum.iloc[factor, 0])/2)
+        
         return 1;
-                
     
 if __name__ == "__main__":
     oacd = OACD();
-    oacd.set_factor_num(5);
-    oacd.set_table_size("Small");
+    oacd.set_factor_num(10);
+    oacd.set_table_size("Large");
     built = oacd.build_table();
 
     print(oacd.table)
-    # array = df.values.tolist()
-    # print(array[:][3])
     
